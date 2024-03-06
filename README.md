00912f8aad10b436d8a16701e500ffcd13e381fa
raina <raina@rainascherer.com>
2024-03-05 16:28:14 -0800

    Update readme and add fill script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # INFO430_final
 Create and populate team 6 (Expedia) INFO430 Database
+
+# Description
+
+This repo contains two SQL files:
+`buildDatabase_expedia.sql` : Builds the tables, check constraints, computed columns, and stored procedures
+`populateDatabase_expedia.sql`: Fills the tables 
+
+The team ERD can be [viewed at this link](https://lucid.app/lucidchart/e0120957-10fd-4c32-bbce-a2cf819ab03d/edit?invitationId=inv_20e5d7eb-ca6e-4ab5-9730-0225f46e1906&page=0_0#)
---
+++ populateDatabase_expedia.sql
@@ -0,0 +1,7 @@
+----------------------------------------------------------------------------------------------------
+-- POPULATE SCRIPT FOR EXPEDIA DATABASE
+----------------------------------------------------------------------------------------------------
+/*
+This only handles insert procedures so that you can just TRUNCATE any tables you want to re-fill, without worrying about re-running anything else.
+*/
+----------------------------------------------------------------------------------------------------
